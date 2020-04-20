822dbdc86375c8f6e24d90891d10a6d7cc6d335a
Frank <fvandykjr@hotmail.com>
2020-04-20 11:01:37 -0700

    First Commit
    Player movement
    Camera movement
    Color Changes

---
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float ForSpeed;
+
+    private Rigidbody PlayerRB;
+    private GameObject FocalPoint;
+    private Renderer PlayerRend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerRB = GetComponent<Rigidbody>();
+        PlayerRend = GetComponent<Renderer>();
+        FocalPoint = GameObject.Find("Focal Point");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float VertIn = Input.GetAxis("Vertical");
+        PlayerRB.AddForce(FocalPoint.transform.forward * VertIn * ForSpeed);
+        if (VertIn > 0)
+        {
+            PlayerRend.material.color = new Color(1 - VertIn, 1, 1 - VertIn);
+        }
+        else
+        {
+            PlayerRend.material.color = new Color(1 + VertIn, 1, 1 + VertIn);
+        }
+        
+
+    }
+}
---
+++ Assets/Course Library/Scripts/RotateCam.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCam : MonoBehaviour
+{
+
+    public float RotSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float HorzIn = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, HorzIn * Time.deltaTime * RotSpeed);
+    }
+}
